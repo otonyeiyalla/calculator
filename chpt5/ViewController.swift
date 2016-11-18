abb84c36b14bc0e87a9d9b509ca845be23b49cc0
Iyalla Otonye <iyallaotonye@dhcp-132-53.cu-wireless-students.carleton.ca>
2016-11-18 17:55:51 -0500

    Basic Calculator

--- chpt5/ViewController.swift
+++ chpt5/ViewController.swift
@@ -8,7 +8,56 @@
 
 import UIKit
 
+enum modes {
+    case NOT_SET
+    case ADDITION
+    case SUBTRACTION
+}
+
 class ViewController: UIViewController {
+    
+    var labelString:String = "0"
+    var currentMode: modes = modes.NOT_SET
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
+    @IBOutlet weak var label: UILabel!
+    @IBAction func tapped0(sender: AnyObject) {tappedNumber(0)}
+    @IBAction func tapped1(sender: AnyObject) {tappedNumber(1)}
+    @IBAction func tapped2(sender: AnyObject) {tappedNumber(2)}
+    @IBAction func tapped3(sender: AnyObject) {tappedNumber(3)}
+    @IBAction func tapped4(sender: AnyObject) {tappedNumber(4)}
+    @IBAction func tapped5(sender: AnyObject) {tappedNumber(5)}
+    @IBAction func tapped6(sender: AnyObject) {tappedNumber(6)}
+    @IBAction func tapped7(sender: AnyObject) {tappedNumber(7)}
+    @IBAction func tapped8(sender: AnyObject) {tappedNumber(8)}
+    @IBAction func tapped9(sender: AnyObject) {tappedNumber(9)}
+    @IBAction func tappedPlus(sender: AnyObject) {//savedNum = Int(labelString)       // labelString = "\(0)"     //  updateText()
+    }
+    @IBAction func tappedMinus(sender: AnyObject) {
+    }
+    @IBAction func tappedEquals(sender: AnyObject) {
+    }
+    @IBAction func tappedClear(sender: AnyObject) {
+        labelString = "\(0)"
+        updateText()
+    }
+    
+    func tappedNumber(num:Int){
+        labelString = labelString.stringByAppendingString("\(num)")
+        updateText()
+    }
+    func updateText() {
+        guard let labelInt:Int = Int(labelString) else {
+            label.text = "Convertion to Int failed"
+            return
+        }
+        label.text = "\(labelInt)"
+    }
+    
+    func changeMode(newMode: modes){
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
